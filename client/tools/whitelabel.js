c92bdee93897a9f7a99f484cac20e6a1da14725a
Ricky Ng-Adam <rngadam@gmail.com>
2015-12-13 18:58:23 +0800

    return config based on current url

--- client/tools/whitelabel.js
+++ client/tools/whitelabel.js
@@ -1,3 +1,12 @@
+function getCurrentConfiguration() {
+  var currentUrl = Router.current().originalUrl;
+  if(currentUrl.startsWith("http://localhost") || currentUrl.startsWith('https://members.hackacademy.org')) {
+    return {appName: 'HackAcademy.org'};
+  } else if(currentUrl.indexOf('codersfield')) {
+    return {appName: 'Codersfield'};
+  }
+}
+
 UI.registerHelper('appName', function(context, options) {
-  return 'HackAcademy.org';
+  return getCurrentConfiguration().appName;
 });
