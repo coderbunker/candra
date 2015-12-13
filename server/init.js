48ac1b853c7ead84a4fe32dd097133c32e779921
Ricky Ng-Adam <rngadam@gmail.com>
2015-12-13 15:56:03 +0800

    add github config

--- server/init.js
+++ server/init.js
@@ -1,11 +1,22 @@
 //console.log(process.env);
 ServiceConfiguration.configurations.upsert(
-  { service: "weibo" },
+  { service: "google" },
   {
     $set: {
       clientId: Meteor.settings.google.clientid,
       loginStyle: "popup",
       secret: Meteor.settings.google.clientsecret,
+    }
+  }
+);
+
+ServiceConfiguration.configurations.upsert(
+  { service: "github" },
+  {
+    $set: {
+      clientId: Meteor.settings.github.clientid,
+      loginStyle: "popup",
+      secret: Meteor.settings.github.clientsecret,
     }
   }
 );
