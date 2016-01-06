010538d006b4d24e44e4b5f2501070e3ef57f66b
Julien Choulet <jchoulet@blue-reef-ltd.com>
2016-01-05 20:11:30 -0500

    improve fetching organization (so that server code does not block inbound client requests)

--- app/server/init.js
+++ app/server/init.js
@@ -1,47 +1,55 @@
 function oauthConfig(service, clientId, secret) {
-   ServiceConfiguration.configurations.upsert(
-    { service: service },
+  ServiceConfiguration.configurations.upsert(
+    {service: service},
     {
       $set: {
         clientId: clientId,
         loginStyle: "popup",
-        secret: secret,
+        secret: secret
       }
     }
-  ); 
+  );
 }
 
 function initOrgs() {
   var directory = Meteor.http.call("GET", "http://spaceapi.net/directory.json?api=0.13");
-  var urls = Object.keys(directory.data).map(function(k) { 
-    var exist = App.Collections.Orgs.findOne({space: k});
-    if(!exist) {
-      try {
-        var url = directory.data[k];
-        console.log('fetching ' + url);
-        var result = Meteor.http.call("GET", url);
-        App.Collections.Orgs.upsert({space: result.data.space}, result.data);
-        return result.data;
-        console.log(e);
-      } catch(e) {
-        console.log(e);
-      }
+  var urls = Object.keys(directory.data).map(function (k) {
+
+    if (!App.Collections.Orgs.findOne({space: k})) {
+      // Execute http calls asynchronously so we don't block execution for client inbound client request
+      // Meteor.defer = Meteor.setTimeout(x, 0)
+      var url = directory.data[k];
+
+      Meteor.defer(function () {
+        try {
+          console.log('fetching ' + url);
+          var result = Meteor.http.call("GET", url);
+          console.log(result.data);
+          App.Collections.Orgs.upsert({space: result.data.space}, result.data);
+
+        } catch (e) {
+          console.log(e);
+        }
+      });
+      return url;
     } else {
-      return exist;
+      return '';
     }
   });
+
+  return _.compact(urls);
 }
 
-Meteor.startup(function() {
+Meteor.startup(function () {
   var config = getCurrentConfiguration();
 
-  if(config.github) {
+  if (config.github) {
     oauthConfig('github', config.github.clientId, config.github.secret);
   }
 
-  if(config.github) {
+  if (config.github) {
     oauthConfig('google', config.google.clientId, config.google.secret);
   }
 
-  //console.log(initOrgs());
+  console.log(initOrgs());
 });
