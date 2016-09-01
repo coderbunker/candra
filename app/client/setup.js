49b7f4d3b6388a18051f5fdd5ded20bb50845846
Ricky Ng-Adam <rngadam@gmail.com>
2016-09-01 16:55:56 +0800

    need flow-routing

--- app/client/setup.js
+++ app/client/setup.js
@@ -1,5 +1,7 @@
+console.log(Object.keys(Package));
+Package['accounts-password'] = undefined;
+
 Meteor.startup(() => {
   AutoForm.setDefaultTemplate("semanticUI");
-  console.log(Object.keys(Package));
-  Package['accounts-password'] = undefined;
+
 });
