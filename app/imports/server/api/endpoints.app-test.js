b087e0da3f46dd5c5e97d1726ecb625b7ecd0c61
thimo visser <thimo.visser@hotmail.nl>
2016-09-07 12:01:56 +0800

    Added correct API call to app tests

--- app/imports/server/api/endpoints.app-test.js
+++ app/imports/server/api/endpoints.app-test.js
@@ -4,61 +4,77 @@ import { chai } from 'meteor/practicalmeteor:chai';
 var assert = chai.assert,
     expect = chai.expect;
 
-describe('API', function () {
-  it('no router', function (done) {
-    HTTP.call('POST', Meteor.absoluteUrl() + 'routerapi/v1/arp/unknown', {}, 
-    function(err, res) { 
-      assert.deepEqual(res.data, { 
-        success: false,
-        message: 'this router does not exist in configuration'
-      });
-      assert.equal(res.statusCode, 404);
-      done();
-    });
-  });
+const CorrectTable = 'IP address       HW type     Flags       HW address            Mask     Device\r\n192.168.1.11     0x1         0x2         8c:3a:e3:93:83:93     *        br0\r\n192.168.1.50     0x1         0x2         00:26:bb:07:54:80     *        br0\r\n192.168.1.18     0x1         0x2         3c:15:c2:cc:dc:2a     *        br0';
 
-  it('no authorization token', function (done) {
-    HTTP.call('POST', Meteor.absoluteUrl() + 'routerapi/v1/arp/candra', {}, 
-      function(err, res) {
-        assert.deepEqual(res.data, { 
-          success: false,
-          message: 'authorization token missing'
-        });
-        assert.equal(res.statusCode, 401);
-        done();
-    });
-  });
+describe('API', function() {
 
-  it('wrong authorization key', function (done) {
-    HTTP.call('POST', Meteor.absoluteUrl() + 'routerapi/v1/arp/candra', { 
-      headers: {
-        Authorization: 'Bearer WRONG'
-      }
-    }, 
-      function(err, res) {
-        assert.deepEqual(res.data, { 
-          success: false,
-          message: 'wrong authorization key'
-        });
-        assert.equal(res.statusCode, 401);
-        done();
+    it('accepts correct call', function() {
+        HTTP.call(
+            'POST',
+            Meteor.absoluteUrl() + 'routerapi/v1/arp/candra', 
+            { headers: { 'Authorization': 'Bearer abcd' }, data: { arpTable: CorrectTable } },
+            function(error, result) {
+                assert(!error, 'there was an error: \n' + error);
+                assert(result.statusCode == 200, 'Status code didn\'t match 200 it was ' + result.statusCode);
+                assert(App.Collections.ARPEntries.find().count() >= 3, 'test addresses were not added successfully')
+            }
+        );
     });
-  });
 
-  it('no data', function (done) {
-    console.log(Meteor.absoluteUrl());
-    HTTP.call('POST', Meteor.absoluteUrl() + 'routerapi/v1/arp/candra', {
-      headers: {
-        Authorization: 'Bearer abcd'
-      }
-    }, 
-      function(err, res) {
-        assert.deepEqual(res.data, { 
-          success: false,
-          message: 'invalid request, missing arpTable'
-        });
-        assert.equal(res.statusCode, 400);
-        done();
+    it('no router', function(done) {
+        HTTP.call('POST', Meteor.absoluteUrl() + 'routerapi/v1/arp/unknown', {},
+            function(err, res) {
+                assert.deepEqual(res.data, {
+                    success: false,
+                    message: 'this router does not exist in configuration'
+                });
+                assert.equal(res.statusCode, 404);
+                done();
+            });
     });
-  });
+
+    it('no authorization token', function(done) {
+        HTTP.call('POST', Meteor.absoluteUrl() + 'routerapi/v1/arp/candra', {},
+            function(err, res) {
+                assert.deepEqual(res.data, {
+                    success: false,
+                    message: 'authorization token missing'
+                });
+                assert.equal(res.statusCode, 401);
+                done();
+            });
+    });
+
+    it('wrong authorization key', function(done) {
+        HTTP.call('POST', Meteor.absoluteUrl() + 'routerapi/v1/arp/candra', {
+                headers: {
+                    Authorization: 'Bearer WRONG'
+                }
+            },
+            function(err, res) {
+                assert.deepEqual(res.data, {
+                    success: false,
+                    message: 'wrong authorization key'
+                });
+                assert.equal(res.statusCode, 401);
+                done();
+            });
+    });
+
+    it('no data', function(done) {
+        console.log(Meteor.absoluteUrl());
+        HTTP.call('POST', Meteor.absoluteUrl() + 'routerapi/v1/arp/candra', {
+                headers: {
+                    Authorization: 'Bearer abcd'
+                }
+            },
+            function(err, res) {
+                assert.deepEqual(res.data, {
+                    success: false,
+                    message: 'invalid request, missing arpTable'
+                });
+                assert.equal(res.statusCode, 400);
+                done();
+            });
+    });
 });
--- app/server/api/endpoints.js
+++ app/server/api/endpoints.js
@@ -1,168 +1,174 @@
 RouterApi = new Restivus({
-  apiPath: 'routerapi/',
-  version: 'v1'
+    apiPath: 'routerapi/',
+    version: 'v1'
 });
 
-RouterApi.addRoute(
-  'arp/:name',
-  {
-    authRequired: false, 
-    prettyJson: true,
-    defaultHeaders: { 'Content-Type': 'application/json' }
-  },
-  {
-    post() {
-
-      var ARPEntries = App.Collections.ARPEntries;
-      var APICalls = App.Collections.APICalls;
-      var request = this.request;
-      var response = this.response;
-      var key, arpTable, headers = request.headers;
-      var routers = App.routers;
-      var router = _.findWhere(routers, {name: this.urlParams.name});
-
-      if (!router) {
-        return {
-          statusCode: 404,
-          body: {
-            success: false, 
-            message: 'this router does not exist in configuration'
-          }
-        };
-      }
-
-      if (!headers.authorization) {
-        return {
-          statusCode: 401,
-          body: {
-            success: false, 
-            message: 'authorization token missing'            
-          }
-        };
-      }
-
-      var key = headers.authorization.substring(7);
-
-      if (key !== router.key) {
-        return {
-          statusCode: 401,
-          body: {
-            success: false, 
-            message: 'wrong authorization key'            
-          }
-        };
-      }
-
-      if(!request.body.arpTable) {
-        return {
-          statusCode: 400,
-          body: {
-            success: false, 
-            message: 'invalid request, missing arpTable'            
-          }
-        };        
-      }
-
-      APICalls.upsert({api: 'arp'}, { $set: {
-          api: 'arp', 
-          time: new Date(), 
-          clientIP: request.headers['x-forwarded-for']        
-        }
-      });
-
-      var arpTable = request.body.arpTable;
-
-      var rows = arpTable.split("\n");
-      var entries = [];
-
-      rows.forEach((row, index) => {
-        if (index === 0) 
-          return;
+var getEntries = function(arpTable) {
+    console.log('processing arp table \n' + arpTable);
+    entries = [];
+    var rows = arpTable.split("\n");
+    rows.forEach((row, index) => {
+        if (index === 0)
+            return;
         var values;
         row = row.replace(/ +(?= )/g, '');
         values = row.split(" ");
 
         // Prevents empty rows from causing errors
-        if (_.isEmpty(values) || values.length !== 6) 
-          return;
+        if (_.isEmpty(values) || values.length !== 6)
+            return;
 
         entries.push({
-          updatedAt: new Date(),
-          IP: values[0],
-          MAC: values[3].toUpperCase()
+            updatedAt: new Date(),
+            IP: values[0],
+            MAC: values[3].toUpperCase()
         });
-      });
+    });
+    return entries;
+};
 
-      entries.forEach((entry) => {
+export {getEntries};
 
-        var ARPEntry = ARPEntries.findOne({MAC: entry.MAC});
+RouterApi.addRoute(
+    'arp/:name', {
+        authRequired: false,
+        prettyJson: true,
+        defaultHeaders: { 'Content-Type': 'application/json' }
+    }, {
+        post() {
 
-        // A new user connected to the router
-        // If MAC address doesn't exist in mongodb, add it
-        if (!ARPEntry) {
+            var ARPEntries = App.Collections.ARPEntries;
+            var APICalls = App.Collections.APICalls;
+            var request = this.request;
+            var response = this.response;
+            var key, arpTable, headers = request.headers;
+            var routers = App.routers;
+            var router = _.findWhere(routers, { name: this.urlParams.name });
 
-          var response;
+            if (!router) {
+                return {
+                    statusCode: 404,
+                    body: {
+                        success: false,
+                        message: 'this router does not exist in configuration'
+                    }
+                };
+            }
 
-          try {
-            response = HTTP.call("POST", "http://api.macvendors.com/" + entry.MAC);
-          }
-          catch (e) {
-            // Got a network error, time-out or HTTP error in the 400 or 500 range.
-            // Do nothing continue
-          }
+            if (!headers.authorization) {
+                return {
+                    statusCode: 401,
+                    body: {
+                        success: false,
+                        message: 'authorization token missing'
+                    }
+                };
+            }
 
-          if (response && response.content) {
-            entry.company = response.content;
-          } else {
-            console.log('error: could not get response from macvendors');
-            console.log(response);
-          }
+            var key = headers.authorization.substring(7);
 
-          ARPEntries.insert(entry);
+            if (key !== router.key) {
+                return {
+                    statusCode: 401,
+                    body: {
+                        success: false,
+                        message: 'wrong authorization key'
+                    }
+                };
+            }
+
+            if (!request.body.arpTable) {
+                return {
+                    statusCode: 400,
+                    body: {
+                        success: false,
+                        message: 'invalid request, missing arpTable body: \n ' + JSON.stringify(request.body)
+                    }
+                };
+            }
+
+            APICalls.upsert({ api: 'arp' }, {
+                $set: {
+                    api: 'arp',
+                    time: new Date(),
+                    clientIP: request.headers['x-forwarded-for']
+                }
+            });
+
+            var arpTable = request.body.arpTable;
+            console.log(arpTable);
+            
+            var entries = getEntries(arpTable);
+            console.log(entries);
+
+
+            entries.forEach((entry) => {
+
+                var ARPEntry = ARPEntries.findOne({ MAC: entry.MAC });
+
+                // A new user connected to the router
+                // If MAC address doesn't exist in mongodb, add it
+                if (!ARPEntry) {
+
+                    var response;
+
+                    try {
+                        response = HTTP.call("POST", "http://api.macvendors.com/" + entry.MAC);
+                    } catch (e) {
+                        // Got a network error, time-out or HTTP error in the 400 or 500 range.
+                        // Do nothing continue
+                    }
+
+                    if (response && response.content) {
+                        entry.company = response.content;
+                    } else {
+                        console.log('error: could not get response from macvendors');
+                        console.log(response);
+                    }
+
+                    ARPEntries.insert(entry);
+                }
+
+                // The user changed IP address
+                // If MAC address exists in mongodb and different IP address, update entry
+                if (ARPEntry) {
+                    if (ARPEntry.IP !== entry.IP) {
+                        ARPEntries.update(ARPEntry, { $set: { IP: entry.IP } });
+                    }
+                }
+            });
+
+            // The user is not connected to the router anymore
+            // Clean up mongodb: if MAC address is in mongodb but not in router table, remove it from mongodb
+            var currentMAC = _.map(ARPEntries.find().fetch(), (entry) => entry.MAC);
+            var newMAC = _.map(entries, (entry) => entry.MAC);
+            var MACtoRemove = _.difference(currentMAC, newMAC);
+
+            MACtoRemove.forEach((MAC) => {
+                ARPEntries.remove({ MAC: MAC });
+            });
+
+            APICalls.update({ api: 'arp' }, { $set: { success: true } });
+
+            return { success: true, message: 'successfully retrieved data' };
+
         }
-
-        // The user changed IP address
-        // If MAC address exists in mongodb and different IP address, update entry
-        if (ARPEntry) {
-          if (ARPEntry.IP !== entry.IP) {
-            ARPEntries.update(ARPEntry, {$set: {IP: entry.IP}});
-          }
-        }
-      });
-
-      // The user is not connected to the router anymore
-      // Clean up mongodb: if MAC address is in mongodb but not in router table, remove it from mongodb
-      var currentMAC = _.map(ARPEntries.find().fetch(), (entry) => entry.MAC);
-      var newMAC = _.map(entries, (entry) => entry.MAC);
-      var MACtoRemove = _.difference(currentMAC, newMAC);
-
-      MACtoRemove.forEach((MAC) => {
-        ARPEntries.remove({MAC: MAC});
-      });
-
-      APICalls.update({api: 'arp'}, {$set: {success: true}});
-
-      return { success: true, message: 'successfully retrieved data' };
-
     }
-  }
 );
 
 SpaceApi = new Restivus({
-  apiPath: 'spaceapi/',
-  version: 'v1'
+    apiPath: 'spaceapi/',
+    version: 'v1'
 });
 
 SpaceApi.addRoute(
-  'status',
-  {
-    authRequired: false,
-    prettyJson: true
-  },
-  {
-    get () {
-      this.response.setHeader('Content-Type', 'application/json');
-      this.response.end(Meteor.settings.public.spaceapi);
+    'status', {
+        authRequired: false,
+        prettyJson: true
+    }, {
+        get() {
+            this.response.setHeader('Content-Type', 'application/json');
+            this.response.end(Meteor.settings.public.spaceapi);
+        }
     }
-  }
 );
