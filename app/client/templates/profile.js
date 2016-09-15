0ad47ed7939f1bce0d76bd6ed291cf31ab6a5e39
thimo visser <thimo.visser@hotmail.nl>
2016-09-15 16:06:52 +0800

    Now fetches the name of the devices, not using proper projection

--- app/client/templates/profile.js
+++ app/client/templates/profile.js
@@ -1,10 +1,25 @@
 Template.profile.onCreated(function () {
     Meteor.subscribe('Meteor.arptable');
+    Meteor.subscribe('Meteor.users');
 });
 
 Template.profile.helpers({
 	macs: function(IP){
-        result = App.Collections.ARPEntries.find({"IP" : IP[0]});
+        cursor = App.Collections.ARPEntries.find({"IP" : IP[0]});
+        result = [];
+
+        cursor.map(entry => {
+        	//TODO: use proper projection
+        	var user = Meteor.users.findOne({'profile.devices.MAC': entry.MAC });
+        	var name;
+        	user.profile.devices.map(MACentry => {
+        		if(MACentry.MAC == entry.MAC){
+        			name = MACentry.name;
+        		}
+        	})
+        	result.push({'MAC': entry.MAC, 'name': name});
+        });
+
         return result;
 	}
 });
